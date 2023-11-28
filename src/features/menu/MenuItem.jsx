c8097b84c1725b0a41783dfc838ef4dc13d39440
coder-guy1 <abhijeetingh11705@gmail.com>
2023-11-28 13:09:04 +0530

    createOrder

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,10 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { formatCurrency } from "../../utils/helpers";
+
+// eslint-disable-next-line react/prop-types
 function MenuItem({ pizza }) {
+  // eslint-disable-next-line react/prop-types
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
@@ -6,7 +12,7 @@ function MenuItem({ pizza }) {
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
---
+++ src/ui/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+
+      <SearchOrder />
+      <Username />
+    </header>
+  );
+}
+
+export default Header;
